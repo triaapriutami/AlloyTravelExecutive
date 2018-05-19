34c1778927f94d7cf006abbd57cb8466542e7c21
Tria <triaapriutami95@gmail.com>
2018-05-20 00:33:27 +0700

    init

--- app/src/main/java/mobile/skripsi/alloytravelexecutive/PesanTiket2Activity.java
+++ app/src/main/java/mobile/skripsi/alloytravelexecutive/PesanTiket2Activity.java
@@ -1,13 +1,47 @@
 package mobile.skripsi.alloytravelexecutive;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class PesanTiket2Activity extends AppCompatActivity {
+
+    private Button buttondp1;
+    private Button buttondp2;
+    private Button buttonselesai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesan_tiket2);
+
+        buttondp1 = (Button) findViewById(R.id.dp1);
+        buttondp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PesanTiket2Activity.this, PesanTiketActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttondp2 = (Button) findViewById(R.id.dp2);
+        buttondp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PesanTiket2Activity.this, PesanTiketActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonselesai =(Button)findViewById(R.id.selesai);
+        buttonselesai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PesanTiket2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
