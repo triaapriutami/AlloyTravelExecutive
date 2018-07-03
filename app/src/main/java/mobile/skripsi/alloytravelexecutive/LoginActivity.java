a40717bdd9fbd65acd7b4824f5dcf6b3b5f8cc94
Tria <triaapriutami95@gmail.com>
2018-07-03 21:47:28 +0700

    init

--- app/src/main/java/mobile/skripsi/alloytravelexecutive/LoginActivity.java
+++ app/src/main/java/mobile/skripsi/alloytravelexecutive/LoginActivity.java
@@ -1,13 +1,26 @@
 package mobile.skripsi.alloytravelexecutive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button buttonlogin;
+    private Button buttonregistrasi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        buttonregistrasi = (Button)findViewById(R.id.registrasi);
+        buttonregistrasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrasiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
