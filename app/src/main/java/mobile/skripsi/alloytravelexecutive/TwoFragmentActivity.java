cea86076490ffab368c5040c6dee9ad8ad96c897
Tria <triaapriutami95@gmail.com>
2018-07-13 12:46:26 +0700

    init

---
+++ app/src/main/java/mobile/skripsi/alloytravelexecutive/TwoFragmentActivity.java
@@ -0,0 +1,26 @@
+package mobile.skripsi.alloytravelexecutive;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TwoFragmentActivity extends Fragment {
+
+    public TwoFragmentActivity() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_two_fragment, container, false);
+    }
+}
